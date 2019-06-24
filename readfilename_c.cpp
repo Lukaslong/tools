249f8e58a4c1676716a39587df53ff18fcebfb9b
Lukaslong <1186842132@qq.com>
2019-06-24 14:03:23 +0800

    将GetFileNames函数直接融合到main函数

--- readfilename_c.cpp
+++ readfilename_c.cpp
@@ -33,3 +33,36 @@ int main()
  
     return 0;
 }
+
+/*
+#include <iostream>
+#include <sys/types.h>
+#include <dirent.h>
+#include <vector>
+#include <string.h>
+
+int main() 
+{
+    std::vector<std::string> file_name;
+    std::string path = "testfile";
+    DIR *pDir;
+    struct dirent* ptr;
+    if(!(pDir=opendir(path.c_str())))
+    	return 1;
+
+    while((ptr=readdir(pDir))!=0)
+    {
+    	if(strcmp(ptr->d_name,".")!=0 && strcmp(ptr->d_name,"..")!=0)
+    		file_name.push_back(path+"/"+ptr->d_name);
+    }
+
+    closedir(pDir);
+
+    for(int i = 0; i <file_name.size(); i++)
+    {
+        std::cout<<file_name[i]<<std::endl;
+    }
+
+    return 0;
+}
+*/
