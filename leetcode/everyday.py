2ec5cf2a8440e5f70d0e0c1ebad0341701fcd693
Lukaslong <1186842132@qq.com>
2020-05-12 06:38:26 +0800

    add 5.12

--- leetcode/everyday.py
+++ leetcode/everyday.py
@@ -44,3 +44,39 @@ class Solution_50_2:
             temp*=temp
             N=N//2
         return ans
+
+# 2020.5.12
+# 最小栈，要求自己定义栈，并完成栈的基本操作函数定义
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.sta=[]
+        
+
+    def push(self, x: int) -> None:
+        self.sta.append(x)
+        
+
+    def pop(self) -> None:
+        self.sta.pop()
+        
+
+    def top(self) -> int:
+        return self.sta[-1]
+        
+
+    def getMin(self) -> int:
+        #minsta=self.sta[0]
+        #for i in range(len(self.sta)):
+        #    minsta=min(minsta,self.sta[i])
+        return min(self.sta)
+        
